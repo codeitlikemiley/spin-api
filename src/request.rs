b0452ee004d28bb4061f916674db0e8d95eef696
Uriah <codeitlikemiley@gmail.com>
2025-02-15 11:52:07 +0800

    remove serialize

--- src/request.rs
+++ src/request.rs
@@ -1,12 +1,12 @@
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use utoipa::ToSchema;
 
-#[derive(ToSchema, Serialize, Deserialize)]
+#[derive(ToSchema, Deserialize)]
 pub(crate) struct CreateTodo {
     pub(crate) title: String,
 }
 
-#[derive(ToSchema, Serialize, Deserialize)]
+#[derive(ToSchema, Deserialize)]
 pub(crate) struct UpdateTodo {
     pub(crate) title: Option<String>,
     pub(crate) completed: Option<bool>,
