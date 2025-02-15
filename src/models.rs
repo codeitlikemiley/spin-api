75e4bf872e1c153dbffc1ed8e4b05a024392dc5f
Uriah <codeitlikemiley@gmail.com>
2025-02-15 09:18:43 +0800

    added from_row

--- src/models.rs
+++ src/models.rs
@@ -3,7 +3,24 @@ use utoipa::ToSchema;
 
 #[derive(ToSchema, Serialize, Deserialize, Clone)]
 pub(crate) struct Todo {
-    pub(crate) id: u64,
-    pub(crate) title: String,
-    pub(crate) completed: bool,
+    pub(super) id: u64,
+    pub(super) title: String,
+    pub(super) completed: bool,
+}
+
+impl Todo {
+    pub(super) fn from_row(row: &spin_sdk::sqlite::Row) -> anyhow::Result<Self> {
+        Ok(Self {
+            id: row
+                .get::<u64>("id")
+                .ok_or_else(|| anyhow::anyhow!("Missing column: id"))?,
+            title: row
+                .get::<&str>("title")
+                .ok_or_else(|| anyhow::anyhow!("Missing column: title"))?
+                .to_string(),
+            completed: row
+                .get::<bool>("completed")
+                .ok_or_else(|| anyhow::anyhow!("Missing column: completed"))?,
+        })
+    }
 }
